5b2ff6b8d2743bb092469b7454d4c77e19f5e262
eloiImt <eloi.garnier24@gmail.com>
2026-03-29 17:25:27 +0200

    Corrections

--- puzzle/vite.config.ts
+++ puzzle/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/PuzzleAnniversaire/',
+  base: process.env.NODE_ENV === 'production' ? '/PuzzleAnniversaire/' : '/',
 })
